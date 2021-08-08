c3955bf69e9b28b582c00e6d42bd7a137bd7b619
KBeyzaDamla <kbeyzadamla02@hotmail.com>
2021-08-08 13:48:39 +0300

    9.Proje
    1 ile 10 arasında rastgele ondalıklı sayı üretip TextBox içinde alt alta ekleme

---
+++ 9.Proje/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _9.Proje
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            int rastgeleTamSayi = r.Next(1, 10);
+            double rastgeleOndalikliSayi = r.NextDouble();
+            double uretilenSayi = rastgeleTamSayi + rastgeleOndalikliSayi;
+            textBox1.Text += uretilenSayi.ToString() + "\r\n";
+        }
+    }
+}
