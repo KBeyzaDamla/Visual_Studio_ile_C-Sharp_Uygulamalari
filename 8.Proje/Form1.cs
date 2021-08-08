0f5206c73ae7b273c41b5983d3b26bed7f9d800a
KBeyzaDamla <kbeyzadamla02@hotmail.com>
2021-08-08 13:47:12 +0300

    8.Proje
    1 ile 20 arasında rastgele tam sayı üretip ListBox içine ekleme

---
+++ 8.Proje/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _8.Proje
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            int rastgeleSayi = r.Next(1, 20);
+            listBox1.Items.Add(rastgeleSayi.ToString());
+        }
+    }
+}
