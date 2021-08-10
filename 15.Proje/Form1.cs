4da71eec84051d20338fd27bb6ebd63c7c2f9aa9
KBeyzaDamla <kbeyzadamla02@hotmail.com>
2021-08-10 17:52:35 +0300

    15.Proje
    TextBox içindeki ilk kelime ile son kelimeyi bulma

---
+++ 15.Proje/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _15.Proje
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int ilkBoslukKarakteri = textBox1.Text.IndexOf(' ');
+            int sonBoslukKarakteri = textBox1.Text.LastIndexOf(' ');
+            string ilkKelime = textBox1.Text.Substring(0, ilkBoslukKarakteri);
+            string sonKelime = textBox1.Text.Substring(sonBoslukKarakteri, textBox1.Text.Length - sonBoslukKarakteri);
+
+            label2.Text = "İlk Boşluk Karakteri = " + ilkBoslukKarakteri.ToString() +
+                "\r\nSon Boşluk Karakteri = " + sonBoslukKarakteri.ToString() +
+                "\r\nİlk Kelime = " + ilkKelime +
+                "\r\nSon Kelime = " + sonKelime;
+        }
+    }
+}
