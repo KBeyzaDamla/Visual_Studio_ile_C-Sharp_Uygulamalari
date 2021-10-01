eaf9683443160e02399a0b1ab23983c5468a4888
KBeyzaDamla <kbeyzadamla02@hotmail.com>
2021-10-01 22:38:03 +0300

    30.Proje
    Girilen bir notun harf karşılığını bulma

---
+++ 30.Proje/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿
+namespace _30.Proje
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lHarfNotu = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Not";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(56, 208);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Harf Karşılığı";
+            // 
+            // lHarfNotu
+            // 
+            this.lHarfNotu.Location = new System.Drawing.Point(56, 249);
+            this.lHarfNotu.Name = "lHarfNotu";
+            this.lHarfNotu.Size = new System.Drawing.Size(159, 37);
+            this.lHarfNotu.TabIndex = 0;
+            this.lHarfNotu.Text = "-";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(56, 74);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(159, 27);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(56, 125);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 47);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Harf Karşılığını Bul";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(270, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lHarfNotu);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lHarfNotu;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ 30.Proje/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _30.Proje
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double not = Convert.ToDouble(textBox1.Text.Replace(".", ","));
+                if (not >= 84)
+                    lHarfNotu.Text = "AA";
+                else if (not < 84 && not >= 77)
+                    lHarfNotu.Text = "AB";
+                else if (not < 77 && not >= 72)
+                    lHarfNotu.Text = "BA";
+                else if (not < 72 && not >= 66)
+                    lHarfNotu.Text = "BB";
+                else if (not < 66 && not >= 61)
+                    lHarfNotu.Text = "BC";
+                else if (not < 61 && not >= 56)
+                    lHarfNotu.Text = "CB";
+                else if (not < 56 && not >= 50)
+                    lHarfNotu.Text = "CC";
+                else if (not < 50 && not >= 46)
+                    lHarfNotu.Text = "CD";
+                else if (not < 46 && not >= 40)
+                    lHarfNotu.Text = "DC";
+                else if (not < 40 && not >= 33)
+                    lHarfNotu.Text = "DD";
+                else if (not < 33 && not >= 0)
+                    lHarfNotu.Text = "FF";
+            }
+            catch (Exception ex)
+            {
+                lHarfNotu.Text = ex.Message;
+            }
+        }
+    }
+}
