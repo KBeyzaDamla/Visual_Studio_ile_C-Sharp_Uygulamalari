1312125733d7827fd5df7686e206f3d16076dec4
KBeyzaDamla <kbeyzadamla02@hotmail.com>
2021-10-01 22:36:43 +0300

    29.Proje
    Metindeki en uzun kelimeyi bulma

---
+++ 29.Proje/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace _29.Proje
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lKelime = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Başlangıç";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(42, 58);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(208, 74);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 243);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "En Uzun Kelime";
+            // 
+            // lKelime
+            // 
+            this.lKelime.Location = new System.Drawing.Point(42, 284);
+            this.lKelime.Name = "lKelime";
+            this.lKelime.Size = new System.Drawing.Size(208, 40);
+            this.lKelime.TabIndex = 0;
+            this.lKelime.Text = "-";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(42, 163);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(208, 54);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "En Uzun Kelimeyi Bul";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(293, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lKelime);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lKelime;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ 29.Proje/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _29.Proje
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] cumle = textBox1.Text.Split(' ');
+            int uzunluk = cumle[0].Length;
+            int uzunKelimeIndisi = 0;
+            
+            for (int i = 0; i < cumle.Length; i++)
+            {
+                if (cumle[i].Length > uzunluk)
+                {
+                    uzunluk = cumle[i].Length;
+                    uzunKelimeIndisi = i;
+                }
+            }
+
+            lKelime.Text = cumle[uzunKelimeIndisi];
+        }
+    }
+}
